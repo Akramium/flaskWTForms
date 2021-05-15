ada19bdf31dca2325b825958ffdf7591be2bc6bc
akramuim <o.akramuim@gmail.com>
2021-05-15 10:38:29 +0000

    Styling with bootstrap, Extends, Imports, Include...

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+Bootstrap(app)
 
 app.secret_key = "AKRAM IS HERE!"
 
